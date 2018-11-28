c5ae11311acf9d251419737a74686c41daa4e991
JAZWii <jazwii2216@gmail.com>
2018-11-28 23:22:40 +0300

    First Push

---
+++ Assets/WM2000/Hacker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+	int level;
+	
+	// Use this for initialization
+	void Start () {
+		Terminal.WriteLine("Hello World!\n\n1-\n2-\n3-");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
